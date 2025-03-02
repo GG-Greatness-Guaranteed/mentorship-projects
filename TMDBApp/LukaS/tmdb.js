74534407276be28afadffb937aaeda91ac1abd6a
Luka Stoiljković <lukastoiljkovic11@gmail.com>
2025-03-02 16:41:37 +0100

    [TMDBApp] Funkcionalnost: Integracija API-ja

--- TMDBApp/LukaS/tmdb.js
+++ TMDBApp/LukaS/tmdb.js
@@ -1,5 +1,48 @@
+const apiKey = '100380da7921e6ab378de2bab017dabb';
 
-/* DROPDOWN MENI ZA MOVIES */
+fetchMovies("popular");
+
+// FETCH ZA FILMOVE
+async function fetchMovies(category) {
+    const url = `https://api.themoviedb.org/3/movie/${category}?api_key=${apiKey}&language=en-US&page=1`;
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        showMovies(data.results);
+        //updatePageUrl(category);
+    } catch (error) {
+        console.error('Error while fetching movies: ', error);
+    }
+}
+
+// PRIKAZ FILMOVA
+function showMovies(movies) {
+    const grid = document.getElementById('grid');
+    grid.innerHTML = ''; // Brišemo postojeće filmove
+
+    movies.forEach(movie => {
+        const movieElement = document.createElement('div');
+        movieElement.classList.add('film');
+
+        const date = new Date(movie.release_date).toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: 'short',
+            day: 'numeric'
+          }); 
+          
+        movieElement.innerHTML = 
+        `
+            <img class="poster" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
+            <div class="filminfo">
+                <p class="title">${movie.title}</p>
+                <p class="date">${date}</p>
+            </div>
+        `;
+        grid.appendChild(movieElement);
+    });
+}
+
+// DROPDOWN MENI ZA MOVIES
 moviesDiv.addEventListener("click", function () {
     if (moviesDropdown.style.display === "block") {
         moviesDropdown.style.display = "none";
@@ -13,4 +56,18 @@ document.addEventListener("click", function (event) {
     }
 });
 
-/* SORT */
+// POZIVANJE FETCH NA OSNOVU KATEGORIJE
+moviesDropdown.addEventListener("click", function (event) {
+    if (event.target.matches("li")) {
+        const category = event.target.id;
+        fetchMovies(category);
+    }
+});
+
+// UPDATOVANJE LINKA STRANICE - TODO
+function updatePageUrl(category) {
+    const newUrl = `${window.location.origin}/tmdb.app/${category}`;
+    history.pushState(null, "", newUrl);
+}
+
+// SORT
